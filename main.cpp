64de51f11cc611f615e1dbce5bf32091bc457b2c
StanaAndrei <44771163+StanaAndrei@users.noreply.github.com>
2019-04-11 12:12:44 +0300

    ytf

--- main.cpp
+++ main.cpp
@@ -5,12 +5,40 @@
 #include <cstdlib>
 #include <ctime>
 #include <conio.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#define ach_len 15
+#define coma_set(shift) std::cout<<fixed<<showpoint<<setprecision(shift)
+#define ullg unsigned long long
 using namespace std;
+vector <char> user_status;
+vector <string> game_name;
+vector <char>::iterator item_status;
+vector <string>::iterator game_item;
+const string g1=" blackjack:",g2=" ruleta:",g3=" dice:";
+#define add_status(g_mode,status) game_name.push_back(g_mode); user_status.push_back(status)
+#define set_tab '\t'
   HANDLE col = GetStdHandle(STD_OUTPUT_HANDLE);///
+  const unsigned short red=1,ver=2,neg=3;
+  unsigned long long vic;
+  static signed v_r[4],v_BJ,v_dice;
 fstream f("bani.out",fstream::in | fstream::out);
 fstream nrt_hand("nrtheme.txt",fstream::in | fstream::out);
-
-int bani,miza=0;
+fstream pro_hand("progres.txt", ios::in | ios::out);
+fstream h_handler("histo.txt",fstream::in | fstream::out);
+fstream h_status("status.txt",fstream::in | fstream::out);
+long double bani,miza=0;
+const char win='w',draw='d',lose='l';
+void update_funds();
+void fix_status();
+void get_stats();
+void update_status();
+void display_stats();
+void display_ach();
+void get_ach();
 void update_theme();
 void sch_theme();
 void welcome();
@@ -21,9 +49,13 @@ void dice();
 int exit();
 int roll();
 void update();
-    short theme;
+    short theme,nr_ach;
+    bool getted[ach_len+1];
+
 
 int main(){
+    system("Title CASINO");
+    get_stats();
     nrt_hand>>theme;
     srand(time(NULL));
     f>>bani;
@@ -32,8 +64,9 @@ int main(){
     f.open("bani.out",fstream::out);
     f << bani;
     f.close();
-    welcome();
-    update_theme();///
+    welcome();///**********
+    get_stats();
+    update_theme();
     menu();
     f.open("bani.out",fstream::out);
     f << bani;
@@ -42,21 +75,33 @@ int main(){
 
 
 void menu(){
-
+    system("cls");
+     cout<<"                              CASINO            "<<endl;
+ cout<<"                        |--------------------------|"<<endl;
+ cout<<"                        |            BY            |"<<endl;
+ cout<<"                        |     GABJEA & $TANA       |"<<endl;
+ cout<<"                        |                     v2.0 |"<<endl;
+ cout<<"                        |--------------------------|"<<endl;//*/
     int r;
     cout << "1.Blackjack\n";
     cout << "2.Ruleta\n";
     cout << "3.Dice\n";
     cout << "4.Exit\n";
     cout<<"5.Schimba tema\n";
-    cout << "\n\nSOLD: " << bani;
+    cout<<"6.Realizarile mele\n";
+    cout<<"7.Istoric\n";
+    cout<<"8.Adauga/Schimba nr de bani\n";
+    cout << "\n\nSOLD: " << bani<<"\n\n\n";
     cout << "\n\nALEGE O OPTIUNE: ";cin>>r;
     switch(r){
         case 1: {blackjack();break;}
         case 2: {ruleta();break;}
         case 3: {dice();break;}
         case 4: {exit();break;}
-        case 5:{sch_theme();break;}///
+        case 5:{sch_theme();break;}
+        case 6:{get_ach();break;}
+        case 7:{display_stats();break;}
+        case 8:{update_funds();break;}
         default: {
 	            system("cls");
             return menu();
@@ -65,6 +110,7 @@ void menu(){
     }
 }
 void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
+    ///***********
     system("cls");
     int carti,player=0,dealer=0,fdown=0,r;
     cout << "\n\nSOLD: "<< bani;
@@ -103,6 +149,7 @@ void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
             cout << "\n\nDealer-ul are Blackjack!!";
             cout << "\n\nDin pacate ai pierdut, mai incearca!";
             cout << "\n\nSOLD: "<< bani;
+           add_status(g1,lose);
         }
         if(player==22){
             player=12;
@@ -115,6 +162,7 @@ void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
             cout << "\n\nDealer-ul are Blackjack!!";
             cout << "\n\nDin pacate ai pierdut, mai incearca!";
             cout << "\n\nSOLD: "<< bani;
+             add_status(g1,lose);
         }
         if(dealer==21){
             system("cls");
@@ -126,6 +174,7 @@ void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
             cout << "\n\nDealer-ul are Blackjack!!";
             cout << "\n\nDin pacate ai pierdut, mai incearca!";
             cout << "\n\nSOLD: "<< bani;
+             add_status(g1,lose);
         }
         if(player==21){
             bani-=miza;
@@ -138,6 +187,8 @@ void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
             cout << "\n\nAi Blackjack!!!";
             cout << "\n\nFelicitari ai castigat!!!";
             cout << "\n\nSOLD: "<< bani;
+         v_BJ++;  ///*****
+          add_status(g1,win);
         }
         while(dealer <=17 && player<21){
             system("cls");
@@ -211,21 +262,29 @@ void blackjack(){ // este un bug in care nu poti alege nimic check it my boy xD
             bani+=2*miza;
             cout << "\nFelicitari ai castigat!!!";
             cout << "\n\nSOLD: "<< bani;
+            add_status(g1,win);
+           ///*************
+           v_BJ++;
         }
         else if(player>21){
             cout << "\n\nDin pacate ai pierdut, mai incearca!";
             cout << "\n\nSOLD: "<< bani;
+             add_status(g1,0);
         }else {
             if(dealer < player){
                 bani+=2*miza;
                 cout << "\nFelicitari ai castigat!!!";
                 cout << "\n\nSOLD: "<< bani;
+                v_BJ++;///************************
+                 add_status(g1,win);
             }
             else if(player < dealer){
                     cout << "\n\nDin pacate ai pierdut, mai incearca!";
                     cout << "\n\nSOLD: "<< bani;
+                    add_status(g1,lose);
             }else {cout << "\n\nEste egalitate.Nimeni nu a castigat!!";
                 bani+=miza;
+                add_status(g1,draw);
             }
 
         }
@@ -316,10 +375,21 @@ void ruleta(){
             else bani+=miza;
             cout << "\nFelicitari ai castigat!!!";
             cout << "\n\nSOLD: "<< bani;
+            ///****************************************************
+          add_status(g2,win);
+            if (opt==red)
+                v_r[red]++;
+            else
+                if (opt==ver)
+                v_r[ver]++;
+            else
+                if (opt==neg)
+                v_r[neg]++;
         }else{
             bani-=miza;
             cout << "\nDin pacate ai pierdut, mai incearca!";
             cout << "\n\nSOLD: "<< bani;
+           add_status(g2,lose);
         }
     }
     update();
@@ -374,10 +444,13 @@ void dice(){
                     bani+=miza;
                     cout << "\nFelicitari ai castigat!!!";
                     cout << "\n\nSOLD: "<< bani;
+                    v_dice++;///************
+                    add_status("dice:",win);
                 }else{
                     bani-=miza;
                     cout << "Din pacate ai pierdut, mai incearca!";
                     cout << "\n\nSOLD: "<< bani;
+                    add_status("dice:",lose);
                 }
                 break;
             }
@@ -387,11 +460,14 @@ void dice(){
                 if(r==nr){
                     bani+=5*miza;
                     cout << "\nFelicitari ai castigat!!!";
-                    cout << "\n\nSOLD: "<< bani;
+                    cout << "\n\nSOLD: "<< bani;///*****
+                  v_dice++;
+                  add_status("dice:",win);
                 }else{
                     bani-=miza;
                     cout << "Din pacate ai pierdut, mai incearca!";
                     cout << "\n\nSOLD: "<< bani;
+                    add_status("dice:",lose);
                 }
                 break;
             }
@@ -400,10 +476,13 @@ void dice(){
                     bani+=miza;
                     cout << "\nFelicitari ai castigat!!!";
                     cout << "\n\nSOLD: "<< bani;
+                    v_dice++;
+                    add_status("dice:",win);
                 }else{
                     bani-=miza;
                     cout << "Din pacate ai pierdut, mai incearca!";
                     cout << "\n\nSOLD: "<< bani;
+                     add_status("dice:",lose);
                 }
                 break;
             }
@@ -434,9 +513,11 @@ void update(){
     f.open("bani.out",fstream::out);
     f << bani;
     f.close();
+
 }
 ///
 int exit(){
+    fix_status();
     system("cls");
     Sleep(400);
     cout << "Exit";
@@ -470,7 +551,6 @@ SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | BACKGROUND_BLUE | BACKGROUND_G
            SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
         system("cls");
         }
-
     }
  SetConsoleTextAttribute(hStdOut, FOREGROUND_RED | BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED);
     while (true) {
@@ -519,4 +599,255 @@ void update_theme()
     }
     system("cls");
     return;
+}///******************************************************
+
+void display_ach()
+{
+
+    size_t cont;
+    double p;
+    system("cls");
+  cout<<"\n Aici sunt realizarile tale,joaca cat mai mult pt a obtine mai multe!(aceste realizari trebuie facute fara sa inchizi casinoul)\n\n";
+string ach_list[ach_len+1];
+ ach_list[0]="  NOROCU INCEPATORULUI (ai castigat o data la BLACJACK in aceasta sesiune)";
+ach_list[1]="  NOVICE (ai castigat de 3 ori la BLACJACK in aceasta sesiune)";
+ach_list[2]="  MAESTRU (ai castigat de 5 ori la BLACJACK in aceasta sesiune)";
+ach_list[3]="  CAMPION PA SATE (ai castigat de 10 ori la BLACJACK in aceasta sesiune)";
+ach_list[4]="  ROSIATICUL (castiga o data la ruleta cu rosu in aceasta sesiune)";
+ach_list[5]="  ROSU ADUCE NOROC (castiga de 3 ori la ruleta cu rosu in aceasta sesiune)";
+ach_list[6]="  FULL RED (castiga de 5 de ori la ruleta cu rosu in aceasta sesiune)";
+ach_list[7]="  VERZISOR (castiga odata la ruleta cu verde in aceasta sesiune)";
+ach_list[8]="  ECOLOGIC (castiga de 3 de ori la ruleta cu verde in aceasta sesiune)";
+ach_list[9]="  NATURAL (castiga de 5 de ori la ruleta cu verde in aceasta sesiune)";
+ach_list[10]="  NEGROTEL (castiga odata la ruleta cu negru in aceasta sesiune)";
+ach_list[11]="  INTUNECATUL (castiga de 3 ori la ruleta cu negru in aceasta sesiune)";
+ach_list[12]="  TIGAN (castiga de 5 de ori la ruleta cu negru in aceasta sesiune)";
+ach_list[13]="  INCEPATOR (castiga odata la dice in aceasta sesiune)";
+ach_list[14]="  NOROCOS (castiga de 3 ori la dice in aceasta sesiune)";
+ach_list[15]="  MAESTRU AL ZARURILOR (castiga de 7 de ori la zaruri in aceasta sesiune)";
+
+ nr_ach=0;///**********
+  for ( cont=0;cont<ach_len;cont++)///
+      if (getted[cont]) {
+           cout<<ach_list[cont]<<endl;
+           nr_ach++;
+  }//*/
+  ///***************************************************************************
+  cout<<"\n Realizari obtinute:";
+  cout<<nr_ach<<"/"<<ach_len+1;
+  p=(float)nr_ach/((float)ach_len+1.0);
+  p*=100.0;
+  coma_set(2);
+  cout<<"  ("<<p<<"%)"<<endl;
+ ullg nr;
+      pro_hand.close();
+        pro_hand.open("progres.txt",fstream::in);
+        pro_hand>>nr;
+         pro_hand.close();
+         cout<<"Ai obtinut toate realizarile de in aceasi tura de "<<nr<<" ori\n";
+ if (nr_ach/ach_len==1) {
+  cout<<"Felicitari ai obtinut toate realizarile in aceasta sesiune!!!"<<endl;
+ ullg x;
+    pro_hand>>x;
+    pro_hand.close();
+    x++;
+    pro_hand.open("progres.txt",fstream::out);
+    pro_hand<<x;
+    pro_hand.close();
+    cout<<" Aceasta e sesiunea cu nr "<<x<<" in care le-ai obtinut pe toate!!!\n"<<endl;
+  }
+    int o;
+cout<<"\n \n 1.Inapoi\n 2.Exit\n";cin>>o;
+ if (o==1)
+ {
+ system("cls");
+ return menu();}
+ else {
+    exit();
+ }
 }
+void get_ach()
+{
+    cout<<"\a";
+ switch (v_BJ)
+{
+ case 1 ... 2:getted[0]=true;break;
+ case 3 ... 4: getted[1]=true;break;
+ case 5 ...9 :getted[2]=true;break;
+ default : if (v_BJ>=10)
+               getted[3]=true;
+}
+for (short i=1;i<4;i++)
+switch (v_r[i])
+{
+    case 1 ... 2:getted[i*3+1]=true;break;
+    case 3 ... 4:getted[i*3+2]=true;break;
+    default : if (v_r[i]>=5)
+                getted[3*(i+1)]=true;
+}
+
+switch (v_dice)
+{
+    case 1 ... 2:getted[13]=true;break;
+    case 3 ... 6:getted[14]=true;;break;
+    default : if (v_dice>=7)
+                 getted[15]=true;
+}
+
+return display_ach();
+}
+///******************
+void get_stats()
+{
+    string g_type;
+    char curr_s;
+  while (h_handler>>g_type)
+game_name.push_back(g_type);
+h_handler.close();
+while (h_status>>curr_s)
+    user_status.push_back(curr_s);
+            return;
+}
+
+void fix_status()
+{
+ h_handler.close();///***********************************************************************
+ h_handler.open("histo.txt",fstream::out);
+ item_status=user_status.begin();
+ game_item=game_name.begin();
+ while (game_item!=game_name.end())
+ {
+     h_handler<<*game_item<<' ';
+     game_item++;
+     //h_handler<<"\n";
+ }
+h_handler.close();
+h_status.close();
+h_status.open("status.txt",fstream::out);
+while (item_status!=user_status.end())
+{
+    h_status<<*item_status<<' ';
+   item_status++;
+}
+h_status.close();
+    return;
+}
+
+void display_stats()
+{///***
+    system("cls");
+ cout<<" Istoric victorii/infrangeri:\n";
+ cout<<" Tip joc";
+ for (int i=0;i<3;i++)///
+    cout<<set_tab;
+ cout<<" Rezultat\n";
+ for (int i=0;i<45;i++)
+    cout<<'-';
+    cout<<"\n\n";
+    ///***/***************************************************//////////////***********
+if (user_status.empty())
+    cout<<" WOW,istoricul e gol , mai joaca si tu ceva!\n";
+item_status=user_status.begin();
+game_item=game_name.begin();
+while (item_status!=user_status.end()&&game_item!=game_name.end())
+    {
+      cout<<*game_item;
+ for (int i=0;i<3;i++)
+    cout<<set_tab;
+ if (*item_status=='w')
+    cout<<"victorie";
+ else
+    if (*item_status=='d')
+     cout<<"remiza";
+  else
+     cout<<"invins";
+        cout<<endl;
+   game_item++;
+   item_status++;
+ }
+ for (int i=0;i<45;i++)
+    cout<<'-';
+
+    ullg games_nr,games_draw,games_win,games_lost;
+    games_nr=user_status.size();
+    games_win=count(user_status.begin(),user_status.end(),win);
+    games_lost=count(user_status.begin(),user_status.end(),lose);
+    games_draw=games_nr-(games_lost+games_win);
+    coma_set(3);
+    float p1,p2,p3=100;
+    p1=(static_cast<float>(games_win)/static_cast<float>(games_nr))*100;
+    p2=(static_cast<float>(games_lost)/static_cast<float>(games_nr))*100;
+    p3-=(p2+p1);
+    cout<<"\n Numar total de meciuri: "<<games_nr<<endl;
+    cout<<" Dintre care:"<<endl;
+    cout<<games_win<<" castigate ";
+    cout<<" ("<<p1<<"%)\n";
+    cout<<games_lost<<" pierdute ";
+    cout<<" ("<<p2<<"%)\n";
+    cout<<games_draw<<" remiza ";
+    cout<<" ("<<p3<<"%)\n";///********************************
+
+    ///*****
+
+  cout<<"\n\n\n";
+      short o;
+      cout<<" Alege o optiune:\n";
+      cout<<" 1.Inapoi"<<endl;
+      cout<<" 2.Sterge istoricul\n";
+      cout<<" 3.Exit"<<endl;
+      coma_set(2);
+      cin>>o;
+      system("cls");
+      if (o==1)
+        return menu();
+  else
+      if (o==2)
+ {
+     user_status.clear();
+     game_name.clear();
+     cout<<"Istoricul se sterge ";
+     for (short i=0;i<4;i++)
+     {
+         cout<<'.';
+         Sleep(1000);
+     }
+     return display_stats();
+ }
+ else
+    if (o==3)
+        exit();
+else
+    return display_stats();
+}
+
+void update_funds()
+{
+    system("cls");
+    cout<<"1.Adauga bani\n";
+    cout<<"2.Schimba nr de bani\n";
+    short o;
+    long double nr_f;
+    cin>>o;
+    system("cls");
+    if (o==1)
+    {
+        cout<<"Cati bani adaugi:";
+        cin>>nr_f;
+        bani+=nr_f;
+    }
+    else
+        if (o==2)
+    {
+        cout<<"Cati bani ai:";
+        cin>>nr_f;
+        bani=nr_f;
+    }
+    else
+        return update_funds();
+     f.close();
+    f.open("bani.out",fstream::out);
+    f << bani;
+    f.close();
+    return menu();
+}
+///******
